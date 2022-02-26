e660a2172029c17b58ede60cc28d82801d119751
Olusola Oseni <kenolusola@gmail.com>
2022-02-26 16:10:42 +0100

    database setup using prisma

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+// import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.contoller';
 import { AuthService } from './auth.service';
 
 @Module({
+  // imports: [PrismaModule], // gives you access to the providers in that module.
   controllers: [AuthController],
   providers: [AuthService],
 })
