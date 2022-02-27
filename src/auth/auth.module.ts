36d80562b536cb3e7d882a375296962dc746469a
Olusola Oseni <kenolusola@gmail.com>
2022-02-27 04:57:33 +0100

    return jwt on signup and signin

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 // import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.contoller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [JwtModule.register({})],
   // imports: [PrismaModule], // gives you access to the providers in that module.
   controllers: [AuthController],
   providers: [AuthService],
