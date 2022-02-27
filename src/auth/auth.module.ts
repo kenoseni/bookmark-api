e7a30e2f7ae4dbc0d91532b39d48f1bed051fd61
Olusola Oseni <kenolusola@gmail.com>
2022-02-27 05:14:41 +0100

    add jwt strategy to decorate routes that needs authorization

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,12 @@ import { JwtModule } from '@nestjs/jwt';
 // import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.contoller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategy';
 
 @Module({
   imports: [JwtModule.register({})],
   // imports: [PrismaModule], // gives you access to the providers in that module.
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
